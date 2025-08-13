d9e389a2474108a48961010d17932f67006f0d8b
Marcus Lim <markerlim15@gmail.com>
2025-08-13 17:41:26 +0800

    modified _error

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,15 +1,13 @@
-import { NextPageContext } from 'next';
-
 export const runtime = 'experimental-edge';
 
 interface ErrorProps {
   statusCode?: number;
 }
 
-function ErrorPage({ statusCode }: ErrorProps) {
+export default function ErrorPage({ statusCode = 404 }: ErrorProps) {
   return (
     <div style={{ textAlign: 'center', padding: '4rem' }}>
-      <h1>{statusCode || 'An error occurred'}</h1>
+      <h1>{statusCode}</h1>
       <p>
         {statusCode
           ? `A ${statusCode} error occurred on the server`
@@ -18,10 +16,3 @@ function ErrorPage({ statusCode }: ErrorProps) {
     </div>
   );
 }
-
-ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
-  const statusCode = res?.statusCode || err?.statusCode || 404;
-  return { statusCode };
-};
-
-export default ErrorPage;
